e10dc9ce94cb5932274c6c426a22002734129112
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-02 13:37:28 +0100

    basic js

---
+++ the-basics/script.js
@@ -0,0 +1,20 @@
+// Variables var, let or const (stores data)
+
+var myName = "Chris";
+console.log(myName); //output Chris
+
+let age = 20;
+console.log(age); //output 20
+console.log(typeof age); //to display what type of data is being displayed
+
+const BRAND = "BMW";
+console.log(BRAND); //output BMW
+
+// Data types
+
+let greeting = "Hello, World!";
+console.log(typeof greeting); //to know what type of data
+
+let isHappy = true;
+console.log(isHappy);
+console.log(typeof isHappy); // displays boolean (yes or no / true or false)
