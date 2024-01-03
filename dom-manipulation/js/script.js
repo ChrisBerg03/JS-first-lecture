43d430c4d508de0b07a0f7fe8baee8147c0bcf6a
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-03 13:40:14 +0100

    array

--- dom-manipulation/js/script.js
+++ dom-manipulation/js/script.js
@@ -34,4 +34,39 @@ console.log(listedItems);
 
 // select the h1 and change color to red
 const heading1 = document.querySelector("h1");
-console.log("first heading: ",heading1);
+console.log("before i change color");
+console.dir(heading1);
+heading1.style.color = "red";
+heading1.style.border = "solid 10px red";
+heading1.style.padding = "1rem";
+heading1.style.backgroundColor = "blue";
+
+
+// select an empty div and give it a class of .container
+const myDiv = document.querySelector("div");
+console.dir(myDiv);
+myDiv.classList.add("container");
+
+// changing the h1 text
+heading1.innerText = "I want beer";
+
+heading2.innerHTML = `<span>I want more beer</span>`;
+
+
+//                        0         1         2         3
+const myArrayOfNames = ["hesh", "kjetil", "jonas", "berkyboi"];
+// console.log(myArrayOfNames[0]);
+// console.log(myArrayOfNames[1]);
+// console.log(myArrayOfNames[2]);
+// console.log(myArrayOfNames[3]);
+
+
+
+for(let i = 0; i < myArrayOfNames.length; i++){
+    console.log(myArrayOfNames[i]);
+};
+
+// i = 0 (the first number)
+// 0 < 3 (if 0 is less than the total number)
+// i = 0 + 1 = 1 (i++ will add 1 and continue to the next)
+// console.log(myArrayOfNames[1]) (will log the arrayOfNames)
