6c005750d29af2a619aaf001f42da4c3a2c45a2c
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-04 11:59:33 +0100

    idk

--- tasks/js/script.js
+++ tasks/js/script.js
@@ -26,7 +26,6 @@ function getRandomNumber(max) {
 
 // addListItem.addEventListener("click", function () {
 //     let someRandomNumber = getRandomInt(220);
-
 //     list.innerHTML += `<li>New Item ${someRandomNumber}</li>`
 //     // list.innerHTML = list.innerHTML +  `<li>New Item</li>`
 // });
