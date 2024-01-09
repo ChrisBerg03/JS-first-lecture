57da7fa2838a37d24c6ba1d3412cfcff46687de0
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-09 14:37:41 +0100

    array objects and functions

--- arrays-objects-functions/js/script.js
+++ arrays-objects-functions/js/script.js
@@ -24,3 +24,89 @@ console.log(numbers);
 
 let animal = "dog";
 console.log(animal.length);
+
+const cars = ["bmw", "audi", "volvo", "Vw"];
+console.log("the index of audi" ,cars.indexOf("audi"));
+
+
+
+
+
+
+// .objects
+
+const user = {
+    name : "Chris",
+    age : 21,
+    job : "Software Engineer",
+    wealth : "Rich"
+};
+
+console.log(user);
+console.log(user.name, user.age);
+
+
+// hello my name is monde and i'm 30 years old, i am rich and i work as a software engineer.
+
+const someText = "hello my name is " + (user.name) + " and i'm " + (user.age) + " years old, i'm " + (user.wealth) + " and i work as a " + (user.job);
+console.log("someText",someText);
+
+
+const someTextTwo = `hello my name is ${user.name} and i'm ${user.age} years old, i'm ${user.wealth} and i work as a ${user.job}`;
+console.log("someTextTwo",someTextTwo);
+
+
+
+const books = [
+    {
+        title: "book one",
+        author: "Autor one",
+        releaseDate: "2001",
+        pages: 300
+    },
+    {
+        title: "book otwo",
+        author: "Autor two",
+        releaseDate: "2003",
+        pages: 500
+    },
+    {
+        title: "book three",
+        author: "Autor three",
+        releaseDate: "2010",
+        pages: 1000
+    }
+];
+
+const bookSearch = books.find(function (book){
+    // return book.title.toLowerCase() === ("book one").toLowerCase();
+    return book.pages >= 300;
+});
+console.log("searchbook",bookSearch);
+
+
+// console.log("my first book",books[0])
+// console.log("my second book",books[1])
+// console.log("my third book",books[2])
+
+
+
+// function is a set of actions or code when we call the functions name.
+
+function doorBell (withCamera) {
+    console.log("i will go and open the door" + withCamera);
+};
+
+doorBell(" Motion detected");
+
+function logWord (theWord){
+    console.log (theWord)
+};
+
+function addTwoNumbers (num1, num2) {
+    return num1 + num2;
+}
+
+const sum = addTwoNumbers(10, 20);
+
+console.log(sum);
