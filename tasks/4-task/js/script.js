499e7f70fd838498574b67b1ce495ce666d0a9f7
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-15 12:12:05 +0100

    task 4

--- tasks/4-task/js/script.js
+++ tasks/4-task/js/script.js
@@ -1,16 +1,15 @@
 const darkModeBtn = document.getElementById("modeBtn");
 
 const body = document.querySelector("body");
-
 let isDarkMode = false;
 
 function toggleMode() {
     isDarkMode = !isDarkMode;
     if (isDarkMode) {
         body.style.backgroundColor = "#0A0A0A";
-        darkModeBtn.style.border = "2px solid #fff"
-        darkModeBtn.style.color = "#000"
-        darkModeBtn.style.backgroundColor = "#fff"
+        darkModeBtn.style.border = "2px solid #fff";
+        darkModeBtn.style.color = "#000";
+        darkModeBtn.style.backgroundColor = "#fff";
     } else {
         body.style.backgroundColor = "#fff";
         darkModeBtn.style.border = "2px solid #000"
