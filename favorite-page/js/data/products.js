3a579f5ee24a64d51b9263faef53c1c5578fe446
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-16 13:22:19 +0100

    a whole load of shit code

---
+++ favorite-page/js/data/products.js
@@ -0,0 +1,32 @@
+export default [
+    {
+    id: 1,
+    name: "Crack deluxe",
+    price: 20
+    },
+    {
+    id: 2,
+    name: "Crack almost deluxe",
+    price: 30
+    },
+    {
+    id: 3,
+    name: "Crack ok",
+    price: 40
+    },
+    {
+    id: 4,
+    name: "Crack minus",
+    price: 50
+    },
+    {
+    id: 5,
+    name: "Crack downgrade",
+    price: 69
+    },
+    {
+    id: 6,
+    name: "Crack aka snow",
+    price: 100
+    },
+]
