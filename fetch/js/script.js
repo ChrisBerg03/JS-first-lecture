b3dc3229b404d8beb62c7bc35ea6b13892a0df8e
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-31 14:12:51 +0100

    fetch fix

--- fetch/js/script.js
+++ fetch/js/script.js
@@ -1,8 +1,16 @@
 // data -- show it to the user
-
+let todos = [];
 let todo = fetch(
     "https://jsonplaceholder.typicode.com/todos",
     function (response) {
         console.log(response);
     }
-);
+)
+    .then((httpResponse) => {
+        // console.log(httpResponse);
+        return httpResponse.json();
+    })
+    .then((todoResult) => {
+        console.log(todoResult);
+        todos = todoResult;
+    });
