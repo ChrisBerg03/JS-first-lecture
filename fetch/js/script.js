9d9ceb95ebda85c596b6febfe2ec0b6434937faa
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-02-01 14:13:03 +0100

    fetch , delete, post,

--- fetch/js/script.js
+++ fetch/js/script.js
@@ -8,10 +8,35 @@ let todo = fetch(
 )
     .then((httpResponse) => {
         // extract the data using JSON
+        console.log(httpResponse);
         return httpResponse.json();
     })
-    .then((todoResult) => {
-        // do something with the data. we store it in the todos variable.
-        console.log(todoResult);
-        todos = todoResult;
+    .then((httpResponse) => {
+        console.log(httpResponse);
     });
+
+fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+    },
+}).then((httpResponse) => console.log(httpResponse));
+
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+    method: "DELETE",
+
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+    },
+}).then((httpResponse) => console.log(httpResponse));
+
+// ---- go fetch a promise to forward to .then.
+
+// ---- await going to fetch
+
+// ---- forward to the next line
