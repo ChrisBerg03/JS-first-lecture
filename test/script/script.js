e9335392083f948a188da472947f7620674aef48
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-02-06 15:29:16 +0100

    height test

--- test/script/script.js
+++ test/script/script.js
@@ -1,11 +1,14 @@
 const num = document.querySelector("#inputField");
 const numBTN = document.querySelector("#inputBTN");
 
-num.addEventListener("input", function () {
-    let val = num.value;
-    if (val <= 5) {
-        console.log("You fail");
+numBTN.onclick = function checkNum() {
+    if (num.value <= 129) {
+        console.log("hehe midget");
+    } else if (num.value <= 159) {
+        console.log("midget+");
+    } else if (num.value <= 179) {
+        console.log("short king");
     } else {
-        console.log("You Win");
+        console.log("hmm, tall ish");
     }
-});
+};
