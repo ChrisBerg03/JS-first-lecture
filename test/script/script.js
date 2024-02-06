c2ee461e5eff46ac90ea9083477fd57c7d1e57dc
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-02-06 15:52:40 +0100

    random ass shit

--- test/script/script.js
+++ test/script/script.js
@@ -1,14 +1,21 @@
 const num = document.querySelector("#inputField");
 const numBTN = document.querySelector("#inputBTN");
+const result = document.querySelector("#outputField");
+const body = document.querySelector("body");
 
 numBTN.onclick = function checkNum() {
     if (num.value <= 129) {
-        console.log("hehe midget");
+        result.innerText = `hehe Midget`;
+        // console.log("hehe midget");
     } else if (num.value <= 159) {
-        console.log("midget+");
+        result.innerText = `Midget+`;
+        // console.log("midget+");
     } else if (num.value <= 179) {
-        console.log("short king");
+        result.innerText = `Short King`;
+        // console.log("short king");
     } else {
-        console.log("hmm, tall ish");
+        result.innerText = `Tall`;
+        body.classList.add("rainbow");
+        alert("You are too tall");
     }
 };
